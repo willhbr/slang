fe07369e60049096e30ba5c3846a7e6bf9949d94
Will Richardson <william.hamish@gmail.com>
2017-12-10 01:50:02 +1300

    Add a super rigorous test

--- spec/slang_spec.cr
+++ spec/slang_spec.cr
@@ -3,5 +3,11 @@ require "./spec_helper"
 describe Slang do
   # TODO: Write tests
 
+  it "runs a file" do
+    r = Runner.new
+    tree = r.read "./spec/test.clj"
+    tree = r.compile tree
+    r.execute tree
+  end
 
 end
